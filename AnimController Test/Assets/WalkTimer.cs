f97971d0158d7cc9e8cebc52002904ddb91b04ae
Christine Yang <christine.yang@yale.edu>
2020-08-13 13:05:52 -0400

    walking script

---
+++ AnimController Test/Assets/WalkTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkTimer : MonoBehaviour
+{
+    public float numSeconds;
+    public string triggername;
+    private Animator anim;
+
+
+    IEnumerator ChangeAnim()
+    {
+        if (!string.IsNullOrEmpty(triggername))
+        {
+            yield return new WaitForSeconds(numSeconds);
+            anim.SetTrigger(triggername);
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(ChangeAnim());
+    }
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
