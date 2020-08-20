e53e160dd4dd702751897def71bcc6a4eb16a614
Christine Yang <christine.yang@yale.edu>
2020-08-20 12:52:57 -0400

    prison guard walk animations

---
+++ SpanishEnvironment1/Assets/Panopticon Assets/AnimTimer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTimer : MonoBehaviour
+{
+    public float numSeconds;
+    public string triggername;
+    private Animator anim;
+
+
+    IEnumerator ChangeAnim()
+    {
+        if (!string.IsNullOrEmpty(triggername))
+        {
+            yield return new WaitForSeconds(numSeconds);
+            anim.SetTrigger(triggername);
+        }
+        
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+        anim = GetComponent<Animator>();
+        StartCoroutine(ChangeAnim());
+        //  anim.ResetTrigger(triggername);
+        // //  foreach(AnimatorControllerParameter p in anim.parameters)
+        // //      {
+        // //         if (p.type == AnimatorControllerParameterType.Trigger)
+        // //         anim.ResetTrigger(p.name);
+        // //      }
+    }
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
