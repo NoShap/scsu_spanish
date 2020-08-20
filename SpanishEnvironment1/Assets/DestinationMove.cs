da18a8e0583d06f2c5c45a7dff0686b8609a83b9
noahs921 <noahshapiro@Noahs-MacBook-Pro.local>
2020-08-20 13:43:16 -0400

    added new Person Pathfinder Animation script

---
+++ SpanishEnvironment1/Assets/DestinationMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DestinationMove : MonoBehaviour
+{
+	private Animator anim;
+	private NavMeshAgent nav;
+	private bool isMoving;
+
+	public List<Transform> destinations;
+	Transform nextTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	anim = GetComponent<Animator>();
+    	nav = GetComponent<NavMeshAgent>();
+    	nav.destination = destinations[0].position;
+    	destinations.RemoveAt(0);
+
+        // nextTransform = destinations[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    	if(nav.remainingDistance <= nav.stoppingDistance)
+    	{
+    		if(destinations.Count != 0)
+    		{
+    			nav.destination = destinations[0].position;
+    			destinations.RemoveAt(0);
+    		}
+    		else
+    		{
+    			isMoving = false;
+    		}
+    		
+    	}
+    	else
+    	{
+    		isMoving = true;
+    	}
+
+    	anim.SetBool("Moving", isMoving);
+
+    }
+
+    void walkForward()
+    {
+
+    }
+}
---
+++ SpanishEnvironment1/Assets/PathFinder.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathFinder : MonoBehaviour
+{
+	private Animator anim;
+	private NavMeshAgent nav;
+	private bool isMoving;
+
+	// public List<Transform> destinations;
+	// transform nextTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	anim = GetComponent<Animator>();
+    	nav = GetComponent<NavMeshAgent>();
+        // nextTransform = destinations[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    	RaycastHit hit;
+
+    	if(Input.GetMouseButtonDown(0))
+    	{
+    		if(Physics.Raycast(ray, out hit, 100))
+    		{
+    			nav.destination = hit.point;
+    		}
+    	}
+
+    	if(nav.remainingDistance <= nav.stoppingDistance)
+    	{
+    		isMoving = false;
+    	}
+    	else
+    	{
+    		isMoving = true;
+    	}
+
+    	anim.SetBool("Moving", isMoving);
+
+    }
+
+    void walkForward()
+    {
+
+    }
+}
