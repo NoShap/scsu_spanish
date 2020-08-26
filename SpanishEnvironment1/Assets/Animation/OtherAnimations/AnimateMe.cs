926b21689b12b481f6597bf90eb49ab0707aa1ff
noahs921 <noahshapiro@Noahs-MacBook-Pro.local>
2020-08-26 12:00:53 -0400

    added new walking scripts and prison cinematic scene

---
+++ SpanishEnvironment1/Assets/Animation/OtherAnimations/AnimateMe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateMe : MonoBehaviour
+{
+    //initialize a general animator variable (this will communicate with our animator window!)
+    private Animator anim;
+    public string animation;
+    
+    void Start()
+    {
+        //store this gameobject's Animator in the variable we just initialized 
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input.GetKet() takes in a string that contains the information 
+        if (Input.GetKey("space")) { 
+            anim.SetBool(animation, true);
+        }
+        // else
+        // {
+        //     anim.SetBool(animation, false);
+        // }
+        
+    }
+}
