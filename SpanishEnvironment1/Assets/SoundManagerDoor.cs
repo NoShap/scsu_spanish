0e7adc3c47d44badc3a275916c15a675f1382965
Zoeth Chalat <zobney@Zoeths-MacBook.local>
2020-08-30 11:15:08 -0400

    Mostly fixed prison in HD_Forest Scene with scripted audio and animations for gates set to f key. For some reason not all gates are opening ¯\_(ツ)_/¯

---
+++ SpanishEnvironment1/Assets/SoundManagerDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManagerDoor : MonoBehaviour
+{
+
+	public AudioSource audio;
+	public Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = gameObject.GetComponent<AudioSource> ();
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update() {
+    	if (Input.GetKey("f")) {
+    		anim.Play("door open", 0, 0f);
+    		audio.PlayOneShot(audio.clip, 1f);
+    		Debug.Log("Played");
+
+    	}
+    }
+
+}
