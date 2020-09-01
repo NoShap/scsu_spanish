af53bf0ce84d756e3db984b9887dbf663f345b38
Christine Yang <christine.yang@yale.edu>
2020-09-01 14:35:38 -0400

    Prelim UI
    Added scene Interaction in Scenes folder; added folder InteractionAssets

---
+++ SpanishEnvironment1/Assets/InteractionAssets/followCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class followCamera : MonoBehaviour
+{
+    public Camera m_Camera;
+ 
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+
+          
+    }
+}
