79543ed8065640e2b70230dd2919c103294675db
noahs921 <noahshapiro@Noahs-MBP.fios-router.home>
2020-09-03 18:04:33 -0400

    walking soliders added

--- SpanishEnvironment1/Assets/DestinationMove.cs
+++ SpanishEnvironment1/Assets/DestinationMove.cs
@@ -5,46 +5,57 @@ using UnityEngine.AI;
 
 public class DestinationMove : MonoBehaviour
 {
-	private Animator anim;
-	private NavMeshAgent nav;
-	private bool isMoving;
+    private Animator anim;
+    private NavMeshAgent nav;
+    private bool isMoving;
 
-	public List<Transform> destinations;
-	Transform nextTransform;
+    public List<Transform> destinations;
+    Transform nextTransform;
     // Start is called before the first frame update
     void Start()
     {
-    	anim = GetComponent<Animator>();
-    	nav = GetComponent<NavMeshAgent>();
-    	nav.destination = destinations[0].position;
-    	destinations.RemoveAt(0);
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+        nav.destination = destinations[0].position;
+        destinations.RemoveAt(0);
 
         // nextTransform = destinations[0];
     }
+
+    // IEnumerator playAnim()
+    // {
+    //     print("coroutine Started");
+    //     yield return new WaitForSeconds(5f);
+    //     print("coroutine continuing");
+
+    // }
 
     // Update is called once per frame
     void Update()
     {
 
-    	if(nav.remainingDistance <= nav.stoppingDistance)
-    	{
-    		if(destinations.Count != 0)
-    		{
-    			nav.destination = destinations[0].position;
-    			destinations.RemoveAt(0);
-    		}
-    		else
-    		{
-    			isMoving = false;
-    		}
-    		
-    	}
-    	else
-    	{
-    		isMoving = true;
-    	}
+        if (nav.remainingDistance <= nav.stoppingDistance)
+        {
+            if (destinations.Count != 0)
+            {
+                nav.destination = destinations[0].position;
+                destinations.RemoveAt(0);
+            }
+            else
+            {
+                isMoving = false;
+            }
 
-    	anim.SetBool("Moving", isMoving);
+        }
+        else
+        {
+            isMoving = true;
+        }
+        if (Input.GetKey("x"))
+        {
+            print("X Hit");
+            anim.SetBool("Moving", isMoving);
+        }
     }
 
 
