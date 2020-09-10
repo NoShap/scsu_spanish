d7f72ec69d3c71178b611bd9d1f7f7e726e4bef9
Noah Shapiro <noahs921@gmail.com>
2020-09-10 15:48:12 -0400

    Noah pes 4 reorganize (#19)
    * walking soliders added
    
    * change to meta files and project lock
    
    * change to meta files and project lockd
    
    * added all textures in assets to a specific Textures File
    
    * added terrain files to Terrain Folder
    
    * missed some textures
    
    * moved materials into material folder
    
    * moved another materials folder into broader  materials folder
    
    * moved several folders into the ImportedAssets Folder and created a new audio folder
    
    * final folders reorganized
    
    * moved scripts and flare asset
    
    Co-authored-by: noahs921 <noahshapiro@Noahs-MBP.fios-router.home>
    Co-authored-by: noahs921 <noahshapiro@Noahs-MacBook-Pro.local>

---
+++ SpanishEnvironment1/Assets/Scripts/DestinationMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DestinationMove : MonoBehaviour
+{
+    private Animator anim;
+    private NavMeshAgent nav;
+    private bool isMoving;
+
+    public List<Transform> destinations;
+    Transform nextTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+        nav.destination = destinations[0].position;
+        destinations.RemoveAt(0);
+
+        // nextTransform = destinations[0];
+    }
+
+    // IEnumerator playAnim()
+    // {
+    //     print("coroutine Started");
+    //     yield return new WaitForSeconds(5f);
+    //     print("coroutine continuing");
+
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (nav.remainingDistance <= nav.stoppingDistance)
+        {
+            if (destinations.Count != 0)
+            {
+                nav.destination = destinations[0].position;
+                destinations.RemoveAt(0);
+            }
+            else
+            {
+                isMoving = false;
+            }
+
+        }
+        else
+        {
+            isMoving = true;
+        }
+        if (Input.GetKey("x"))
+        {
+            print("X Hit");
+            anim.SetBool("Moving", isMoving);
+        }
+    }
+
+
+}
