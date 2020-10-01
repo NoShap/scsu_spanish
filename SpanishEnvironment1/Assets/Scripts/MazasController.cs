9300614b205942019b3f5c148e8a6216bac78299
zchalat <54860747+zchalat@users.noreply.github.com>
2020-10-01 15:10:38 -0400

    Zodi pes hackathon2 (#29)
    * runs on k key press, added sample ui and audio
    
    * fixed on last thing
    
    Co-authored-by: Zoeth Chalat <zobney@Zoeths-MacBook.local>

--- SpanishEnvironment1/Assets/Scripts/MazasController.cs
+++ SpanishEnvironment1/Assets/Scripts/MazasController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MazasController : MonoBehaviour
 {
@@ -12,7 +13,10 @@ public class MazasController : MonoBehaviour
     public GameObject door;
     private bool doorOpened;
     private bool up;
+    private bool waitingForShots;
+    private bool shotsFired = false;
     AnimatorClipInfo[] m_CurrentClipInfo;
+    [SerializeField] Transform finalDest;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +29,6 @@ public class MazasController : MonoBehaviour
     }
 
     // Update is called once per frame
-    [System.Obsolete]
     void Update()
     {
 
@@ -34,34 +37,34 @@ public class MazasController : MonoBehaviour
         {
             if (anim.GetBool("Walking"))
             {
-                print("reached spot walking");
                 print("Remaining:" + nav.remainingDistance);
                 print("Stopping:" + nav.stoppingDistance);
                 destinations.RemoveAt(0);
-                anim.SetBool("Walking", false);
-                nav.Stop();
+                if (destinations.Count > 0)
+                {
+                  nextDestination();
+                }
+                else
+                {
+                    anim.SetBool("Walking", false);
+                    waitingForShots = true;
+                    
+                }
+                
             }
-        }
+            if (anim.GetBool("Running") && shotsFired)
+            {
+                anim.SetBool("Running", false);
+                anim.Play("Terrified");
+                GameObject.Find("UI").GetComponent<Text>().text = "\n Pregúntele a donde va con la frase siquienta: \n ¿A donde va usted?";
+                FindObjectOfType<AudioManager>().Play("DondeVa");  
+            }
 
+        }
         // move Sanchez Mazas
         if (Input.GetKey("m"))
         {
-            if (!doorOpened && up)
-            {
-                doorOpened = true;
-                door.GetComponent<Animator>().Play("door open", 0, 0f);
-                door.GetComponent<AudioSource>().Play();
-            }
-            if (doorOpened)
-            {
-                anim.SetBool("Walking", true);
-                nav.destination = destinations[0].position;
-                nav.Resume();
-                FindObjectOfType<AudioManager>().Play("Move");
-                //Fetch the current Animation clip information for the base layer
-                m_CurrentClipInfo = anim.GetCurrentAnimatorClipInfo(0);
-                anim.Play(m_CurrentClipInfo[0].clip.name, 0, 0.95f);
-            }
+            nextDestination();
         }
 
         if (Input.GetKey("g"))
@@ -77,11 +80,53 @@ public class MazasController : MonoBehaviour
         if (Input.GetKey("c"))
         {
             destinations.RemoveAt(0);
-            if (destinations[0] != null)
-            {
-                nav.destination = destinations[0].position;
-            }
+            anim.SetBool("Running", true);
+            nav.destination = destinations[0].position;
+            print("Hello");
+            
+        }
+        if (Input.GetKeyDown("k"))
+        {
+          StartCoroutine(freakAndRun());
+        }
+        if (Input.GetKeyDown("r"))
+        {
+          anim.SetBool("Running", true);
+          nav.speed = 5f;
+          nav.destination = finalDest.position;
         }
 
+    }
+
+    private void nextDestination()
+    {
+      if (!doorOpened && up)
+            {
+                doorOpened = true;
+                door.GetComponent<Animator>().Play("door open", 0, 0f);
+                door.GetComponent<AudioSource>().Play();
+                //nav.destination = destinations[0].position;
+
+                
+            }
+            anim.SetBool("Walking", true);
+            nav.destination = destinations[0].position;
+            FindObjectOfType<AudioManager>().Play("Move");
+            //SHORTEN
+            //Fetch the current Animation clip information for the base layer
+            m_CurrentClipInfo = anim.GetCurrentAnimatorClipInfo(0);
+            //Access the current length of the clip
+            string m_ClipName = m_CurrentClipInfo[0].clip.name;
+            anim.Play(m_ClipName, 0, 0.9f);
+    }
+
+    IEnumerator freakAndRun()
+    {
+      nav.speed = 8f;
+      nav.destination = finalDest.position;
+      anim.Play("Running");
+      anim.SetBool("Running", true);
+      yield return new WaitForSeconds(3f);
+      shotsFired = true;
     }
 }
