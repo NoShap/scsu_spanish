d0f14ff0fe994872348403afb499ed3d3b0c4bb0
zchalat <54860747+zchalat@users.noreply.github.com>
2020-10-07 15:42:34 -0400

    added dialgue manager (#34)
    Co-authored-by: Zoeth Chalat <zobney@Zoeths-MacBook.local>

---
+++ SpanishEnvironment1/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Text uI;
+    private AudioManager audioManager;
+
+    private int conversationCounter = 0;
+    private string[] conversationLines = {"Diga: Hola, ¿como está?", "\"Tengo hambre\"", "Pregúntele: ¿Que quiere que le traiga?", "\"Comería qualquier cosa\"", "Diga: Ok, le traigo una bandeja de comida"};
+    private string[] audioLines = {"d0", "d1", "d2", "d3", "d4"};
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      uI = GameObject.Find("UI").GetComponent<Text>();
+      audioManager = FindObjectOfType<AudioManager>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown("b"))
+        {
+          StartCoroutine(conversate1());
+        }  
+    }
+    
+    IEnumerator conversate1()
+    {
+      if (conversationCounter < conversationLines.Length)
+      {
+        uI.text = "\n " + conversationLines[conversationCounter];
+        yield return new WaitForSeconds(1f);
+        audioManager.Play(audioLines[conversationCounter]);
+        conversationCounter++;
+      }
+      else
+      {
+        uI.text = "";
+      }
+    }
+}
