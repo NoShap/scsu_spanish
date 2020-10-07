c25c638e1f06b364981a08d5ab5553b8632c6c09
Noah Shapiro <noahs921@gmail.com>
2020-10-07 16:26:07 -0400

    Dialogue in checkpoint manager (#35)
    * added enums and edited scene order
    
    * more changes to enums and scene order
    
    * made final edits to scene and will be adding other dialogue changes
    
    Co-authored-by: noahs921 <noahshapiro@Noahs-MacBook-Pro.local>

--- SpanishEnvironment1/Assets/Scripts/DialogueManager.cs
+++ SpanishEnvironment1/Assets/Scripts/DialogueManager.cs
@@ -10,38 +10,39 @@ public class DialogueManager : MonoBehaviour
     private AudioManager audioManager;
 
     private int conversationCounter = 0;
-    private string[] conversationLines = {"Diga: Hola, ¿como está?", "\"Tengo hambre\"", "Pregúntele: ¿Que quiere que le traiga?", "\"Comería qualquier cosa\"", "Diga: Ok, le traigo una bandeja de comida"};
-    private string[] audioLines = {"d0", "d1", "d2", "d3", "d4"};
-
+    private string[] conversationLines = { "Diga: Hola, ¿como está?", "\"Tengo hambre\"", "Pregúntele: ¿Que quiere que le traiga?", "\"Comería qualquier cosa\"", "Diga: Ok, le traigo una bandeja de comida" };
+    private string[] audioLines = { "d0", "d1", "d2", "d3", "d4" };
+    public static bool dialogueFinished = false;
 
     // Start is called before the first frame update
     void Start()
     {
-      uI = GameObject.Find("UI").GetComponent<Text>();
-      audioManager = FindObjectOfType<AudioManager>();   
+        uI = GameObject.Find("UI").GetComponent<Text>();
+        audioManager = FindObjectOfType<AudioManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown("b"))
+        if (Input.GetKeyDown("b"))
         {
-          StartCoroutine(conversate1());
-        }  
+            StartCoroutine(conversate1());
+        }
     }
-    
+
     IEnumerator conversate1()
     {
-      if (conversationCounter < conversationLines.Length)
-      {
-        uI.text = "\n " + conversationLines[conversationCounter];
-        yield return new WaitForSeconds(1f);
-        audioManager.Play(audioLines[conversationCounter]);
-        conversationCounter++;
-      }
-      else
-      {
-        uI.text = "";
-      }
+        if (conversationCounter < conversationLines.Length)
+        {
+            uI.text = "\n " + conversationLines[conversationCounter];
+            yield return new WaitForSeconds(1f);
+            audioManager.Play(audioLines[conversationCounter]);
+            conversationCounter++;
+        }
+        else
+        {
+            uI.text = "";
+            dialogueFinished = true;
+        }
     }
 }
--- SpanishEnvironment1/Assets/Scripts/checkpointManager.cs
+++ SpanishEnvironment1/Assets/Scripts/checkpointManager.cs
@@ -6,27 +6,43 @@ using UnityEngine.UI;
 
 public class checkpointManager : MonoBehaviour
 {
-    private double currStage = 0f;
+    public enum stage
+    {
+        fadeIn, // screen goes from black to full brightness
+        voiceOver1, //historical intro
+        dialogue1, //talk with Sanchez Mazas
+        voiceOver2, //explaining checkpoints and tasks
+        task1, // get food for Sanchez Mazas 
+        voiceOver3, //explaining languageObserver
+        dialogue2, //dialogue with food delivery guard
+        task2, // pick up food and receive directive to return to SM
+        dialogue3, //talk with Sanchez Mazas 
+        event1, //watching prisoners be escorted out
+        dialogue4, //tell Sanchez Mazas to get up and move outside
+        event2, // Sanchez Mazas runs away
+        dialogue5, // confront Sanchez Mazas
+        voiceOver4, //consequences
+        fadeOut,
+    }
+
+    private stage currStage = stage.fadeIn;
     public GameObject checkpointPrefab;
     private GameObject currCheckpoint;
     public GameObject door;
     private AudioManager audio_manager;
+    private GameObject dialogueManager;
+    private DialogueManager dialogue1;
     bool stageOpen = true;
 
-
-
-    /*
-        Numeric Stages : Player progress in game
-        2 : food delivery
-    */
 
 
     // Start is called before the first frame update
     void Start()
     {
         audio_manager = FindObjectOfType<AudioManager>();
-        currStage = 1f;
-
+        currStage = stage.voiceOver1; //should eventually be set to fadeIn
+        dialogueManager = GameObject.Find("Dialogue1");
+        dialogue1 = dialogueManager.GetComponent<DialogueManager>();
     }
 
     // Update is called once per frame
@@ -35,19 +51,26 @@ public class checkpointManager : MonoBehaviour
     {
         print("current stage: " + currStage);
         // Stage 0
-        if (currStage == 0)
+        if (currStage == stage.fadeIn)
         {
             // Wait for Fade In and then proceed
+            currStage++;
         }
         // Stage 1
-        if (currStage == 1f && stageOpen)
+        if (currStage == stage.voiceOver1 && stageOpen)
         {
             stageOpen = false;
             //call coroutine to play audio file of voiceover
             StartCoroutine(waitForAudioClip("VoiceOver1"));
         }
+        if (currStage == stage.dialogue1 && stageOpen)
+        {
+            //play dialogue through
+            //get something that increments curr stage 
+
+        }
         // Stage 2
-        if (currStage == 2f && stageOpen) //beginning food delivery task
+        if (currStage == stage.voiceOver2 && stageOpen) //beginning food delivery task
         {
             //display text
             GameObject.Find("UI").GetComponent<Text>().text = "\n Proceed to the Food Pickup Area outside the prison.";
@@ -60,55 +83,55 @@ public class checkpointManager : MonoBehaviour
             StartCoroutine(waitForAudioClip("VoiceOver2"));
 
         }
-        //Start interaction with Cook
-        if (currStage == 3f && stageOpen)
-        {
-            if (currCheckpoint.GetComponent<CheckpointTrigger>().hasReached == true) //if player has reached the checkpoint, destroy this instance of the checkpoint prefab and display new instructions
-            {
-                Destroy(currCheckpoint);
-                GameObject.Find("UI").GetComponent<Text>().text = "\n Select the apple and the potatoes and add them to the tray. Press x to continue";
-                stageOpen = false;
-                StartCoroutine(waitForAudioClip("VoiceOver3"));
-            }
-        }
-        if (currStage == 4f)
-        {
-            if (Input.GetKeyDown("x"))
-            {
-                GameObject.Find("UI").GetComponent<Text>().text = "\n Deliver the tray to Sanchez Mazas' prison cell.";
-                //instantiate a checkpoint by Sanchez Mazas' Cell
-                Quaternion rot = Quaternion.Euler(-1f, -28f, 0f);
-                currCheckpoint = Instantiate(checkpointPrefab, new Vector3(134f, 5f, 110f), rot);
-                currCheckpoint.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-                currStage = 5f;
-            }
-        }
-        if (currStage == 5f)
-        {
-            if (currCheckpoint.GetComponent<CheckpointTrigger>().hasReached == true)
-            {
+        // //Start interaction with Cook
+        // if (currStage == 3f && stageOpen)
+        // {
+        //     if (currCheckpoint.GetComponent<CheckpointTrigger>().hasReached == true) //if player has reached the checkpoint, destroy this instance of the checkpoint prefab and display new instructions
+        //     {
+        //         Destroy(currCheckpoint);
+        //         GameObject.Find("UI").GetComponent<Text>().text = "\n Select the apple and the potatoes and add them to the tray. Press x to continue";
+        //         stageOpen = false;
+        //         StartCoroutine(waitForAudioClip("VoiceOver3"));
+        //     }
+        // }
+        // if (currStage == 4f)
+        // {
+        //     if (Input.GetKeyDown("x"))
+        //     {
+        //         GameObject.Find("UI").GetComponent<Text>().text = "\n Deliver the tray to Sanchez Mazas' prison cell.";
+        //         //instantiate a checkpoint by Sanchez Mazas' Cell
+        //         Quaternion rot = Quaternion.Euler(-1f, -28f, 0f);
+        //         currCheckpoint = Instantiate(checkpointPrefab, new Vector3(134f, 5f, 110f), rot);
+        //         currCheckpoint.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        //         currStage = 5f;
+        //     }
+        // }
+        // if (currStage == 5f)
+        // {
+        //     if (currCheckpoint.GetComponent<CheckpointTrigger>().hasReached == true)
+        //     {
 
-                StartCoroutine("GiveFood");
-                GameObject.Find("UI").GetComponent<Text>().text = "\n Task Complete. You may now Explore the Map.";
-                currStage = 6f;
-            }
-        }
-        if (currStage == 6f)
-        {
-            if (Input.GetKeyDown("5")) //to be changed to colliding w/ checkpoint
-            {
-                GameObject.Find("UI").GetComponent<Text>().text = "\n Explore the map.";
-                currStage = 7f;
-            }
-        }
-        if (currStage == 7f)
-        {
-            if (Input.GetKeyDown("6")) //to be changed to colliding w/ checkpoint
-            {
-                Destroy(GameObject.Find("FoodTray"));
-                currStage = 8f;
-            }
-        }
+        //         StartCoroutine("GiveFood");
+        //         GameObject.Find("UI").GetComponent<Text>().text = "\n Task Complete. You may now Explore the Map.";
+        //         currStage = 6f;
+        //     }
+        // }
+        // if (currStage == 6f)
+        // {
+        //     if (Input.GetKeyDown("5")) //to be changed to colliding w/ checkpoint
+        //     {
+        //         GameObject.Find("UI").GetComponent<Text>().text = "\n Explore the map.";
+        //         currStage = 7f;
+        //     }
+        // }
+        // if (currStage == 7f)
+        // {
+        //     if (Input.GetKeyDown("6")) //to be changed to colliding w/ checkpoint
+        //     {
+        //         Destroy(GameObject.Find("FoodTray"));
+        //         currStage = 8f;
+        //     }
+        // }
 
     }
 
@@ -119,7 +142,7 @@ public class checkpointManager : MonoBehaviour
         print("here");
         yield return new WaitForSeconds(aud.length + 1f);
         print("finished");
-        currStage += 1f;
+        currStage++;
         stageOpen = true;
     }
 
