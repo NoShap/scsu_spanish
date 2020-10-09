57d35bbb4d7978b6378442f47a5d2797a3fc5b35
noahs921 <noahshapiro@Noahs-MacBook-Pro.local>
2020-10-09 15:06:00 -0400

    completed dialogue class

--- SpanishEnvironment1/Assets/Scripts/DialogueManager.cs
+++ SpanishEnvironment1/Assets/Scripts/DialogueManager.cs
@@ -6,42 +6,33 @@ using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-    private Text uI;
-    private AudioManager audioManager;
-
-    private int conversationCounter = 0;
-    private string[] conversationLines = {"Diga: Hola, ¿como está?", "\"Tengo hambre\"", "Pregúntele: ¿Que quiere que le traiga?", "\"Comería qualquier cosa\"", "Diga: Ok, le traigo una bandeja de comida"};
-    private string[] audioLines = {"d0", "d1", "d2", "d3", "d4"};
-
-
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] dialogues;
+    private int currActiveDialogue = 0;
+    private bool isRunning = false;
+    private void Start()
     {
-      uI = GameObject.Find("UI").GetComponent<Text>();
-      audioManager = FindObjectOfType<AudioManager>();   
+        foreach (GameObject d in dialogues)
+        {
+            d.SetActive(false);
+        }
+    }
+    IEnumerator playActiveDialogue()
+    {
+        isRunning = true;
+        print("staging: " + currActiveDialogue);
+        dialogues[currActiveDialogue].SetActive(true);
+        print("set active:" + currActiveDialogue);
+        while (!dialogues[currActiveDialogue].GetComponent<Dialogue>().dialogueFinished)
+        {
+            yield return null;
+        }
+        print("Dialogue Completed, moving to next dialogue.");
+        isRunning = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown("b"))
-        {
-          StartCoroutine(conversate1());
-        }  
+        if (!isRunning) StartCoroutine(playActiveDialogue());
     }
-    
-    IEnumerator conversate1()
-    {
-      if (conversationCounter < conversationLines.Length)
-      {
-        uI.text = "\n " + conversationLines[conversationCounter];
-        yield return new WaitForSeconds(1f);
-        audioManager.Play(audioLines[conversationCounter]);
-        conversationCounter++;
-      }
-      else
-      {
-        uI.text = "";
-      }
-    }
+
 }
