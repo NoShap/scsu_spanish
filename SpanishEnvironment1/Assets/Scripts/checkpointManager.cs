fbb752f56e6864ddcc519b67b5078a5ebf011d0e
Christine Yang <christine.yang@yale.edu>
2020-11-02 13:14:56 -0500

    CheckpointManager changes

--- SpanishEnvironment1/Assets/Scripts/checkpointManager.cs
+++ SpanishEnvironment1/Assets/Scripts/checkpointManager.cs
@@ -17,10 +17,10 @@ public class checkpointManager : MonoBehaviour
         // task1, // get food for Sanchez Mazas 
         // voiceOver3, //explaining languageObserver
         // dialogue3, //talk with Food Stand Man 
-        task2, // pick up food and receive directive to return to SM
-        dialogue4, // Tell Sanchez Mazas to eat food
-        event1, //watching prisoners be escorted out
-        dialogue5, //Guard Tells you to get the prisoner
+        // task2, // pick up food and receive directive to return to SM
+        // dialogue4, // Tell Sanchez Mazas to eat food
+        // event1, //watching prisoners be escorted out
+        // dialogue5, //Guard Tells you to get the prisoner
         dialogue6, // tell SM to get up and go outside (and move him outside)
         event2, // SM goes outside
         event3, // Sanchez Mazas runs away and player yells stop
@@ -30,7 +30,7 @@ public class checkpointManager : MonoBehaviour
     }
 
     private GameObject UI;
-    public stage currStage = stage.fadeIn;
+    public stage currStage = stage.event2;
     public GameObject checkpointPrefab;
     public GameObject Guard;
     private GameObject currCheckpoint;
@@ -60,7 +60,7 @@ public class checkpointManager : MonoBehaviour
     void Update()
     {
 
-        // Stage 0
+        // // Stage 0
         if (currStage == stage.fadeIn)
         {
             // Wait for Fade In and then proceed
@@ -76,11 +76,13 @@ public class checkpointManager : MonoBehaviour
         // if (currStage == stage.dialogue1 && stageOpen)
         // {
         //     stageOpen = false;
+        //     UI.GetComponent<Text>().text = "\n Sanchez Mazas looks like he needs to speak to you. Press 7 to listen, and 7 to continue a conversation.";
         //     dialogueManager.startDialogue(0);
         // }
         // if (currStage == stage.dialogue2 && stageOpen)
         // {
         //     stageOpen = false;
+        //     UI.GetComponent<Text>().text = "\n A guard needs to talk to you. Press 7 to listen.";
         //     var guard_mover = Guard.GetComponent<DestinationMove>();
         //     guard_mover.moveToDestination(guard_mover.target);
         //     dialogueManager.startDialogue(1);
@@ -119,57 +121,66 @@ public class checkpointManager : MonoBehaviour
         // {
         //     stageOpen = false;
         //     print("Talk with food stand ");
+        //     UI.GetComponent<Text>().text = "\n Press 7 to speak with the guard on kitchen duty.";
         //     dialogueManager.startDialogue(2);
         // }
-        // having acquired the tray, go deliver it to sanchez mazas
-        if (currStage == stage.task2 && stageOpen)
-        {
-            //stageOpen = false;
-            UI.GetComponent<Text>().text = "\n Deliver the tray to Sanchez Mazas' prison cell.";
-            //instantiate a checkpoint by Sanchez Mazas' Cell
-            Quaternion rot = Quaternion.Euler(-1f, -28f, 0f);
-            currCheckpoint = Instantiate(checkpointPrefab, new Vector3(134f, 5f, 110f), rot);
-            currCheckpoint.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-            currStage += 1;
+        // // having acquired the tray, go deliver it to sanchez mazas
+        // if (currStage == stage.task2 && stageOpen)
+        // {
+        //     //stageOpen = false;
+        //     UI.GetComponent<Text>().text = "\n Deliver the tray to Sanchez Mazas' prison cell.";
+        //     //instantiate a checkpoint by Sanchez Mazas' Cell
+        //     Quaternion rot = Quaternion.Euler(-1f, -28f, 0f);
+        //     currCheckpoint = Instantiate(checkpointPrefab, new Vector3(134f, 5f, 110f), rot);
+        //     currCheckpoint.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        //     currStage += 1;
 
-        }
+        // }
 
-        if (currStage == stage.dialogue4 && stageOpen)
-        {
-            if (currCheckpoint.GetComponent<CheckpointTrigger>().hasReached == true)
-            {
-                stageOpen = false;
-                StartCoroutine("GiveFood");
-                dialogueManager.startDialogue(3);
-                //UI.GetComponent<Text>().text = "\n Task Complete. You may now Explore the Map.";
-            }
-        }
-        // event1 watching prisoners be escorted out
-        if (currStage == stage.event1 && stageOpen)
-        {
-            stageOpen = false;
-            prisoners.GetComponent<MoveOut>().moveOutChildren(prisoners.transform);
-            currStage += 1;
-            stageOpen = true;
-        }
-        //Guard Tells you to get the prisoner
-        if (currStage == stage.dialogue5 && stageOpen)
-        {
-            stageOpen = false;
-            dialogueManager.startDialogue(4);
-        }
+        // if (currStage == stage.dialogue4 && stageOpen)
+        // {
+        //     if (currCheckpoint.GetComponent<CheckpointTrigger>().hasReached == true)
+        //     {
+        //         stageOpen = false;
+        //         StartCoroutine("GiveFood");
+        //         dialogueManager.startDialogue(3);
+        //         UI.GetComponent<Text>().text = "\n Task Complete. Press 7 to speak with Sanchez Mazas.";
+        //     }
+        // }
+        // // event1 watching prisoners be escorted out
+        // if (currStage == stage.event1 && stageOpen)
+        // {
+        //     stageOpen = false;
+        //     UI.GetComponent<Text>().text = "\n The prisoners are being moved outside.";
+        //     var guard_mover = Guard.GetComponent<DestinationMove>();
+        //     guard_mover.moveToDestination(guard_mover.target);
+        //     prisoners.GetComponent<MoveOut>().moveOutChildren(prisoners.transform);
+        //     currStage += 1;
+        //     stageOpen = true;
+        // }
+        // //Guard Tells you to get the prisoner
+        // if (currStage == stage.dialogue5 && stageOpen)
+        // {
+        //     stageOpen = false;
+        //     UI.GetComponent<Text>().text = "\n A guard has a command for you. Press 7 to speak with him.";
+        //     dialogueManager.startDialogue(4);
+        // }
         // tell Sanchez Mazas to get up
         if (currStage == stage.dialogue6 && stageOpen)
         {
             stageOpen = false;
+            UI.GetComponent<Text>().text = "\n Press 7 to relay your command to Sanchez Mazas.";
             dialogueManager.startDialogue(5);
+            UI.GetComponent<Text>().text = "\n Press 7 to let Sanchez Mazas out of the cell.";
             mazasController.getUp();
         }
         // Sanchez Mazas goes outside
         if (currStage == stage.event2 && stageOpen)
         {
             stageOpen = false;
+            UI.GetComponent<Text>().text = "\n Follow Sanchez Mazas outside.";
             mazasController.nextDestination();
+            Debug.Log("waiting");
             waitForTime(10f);
         }
 
@@ -177,14 +188,16 @@ public class checkpointManager : MonoBehaviour
         if (currStage == stage.event3 && stageOpen)
         {
             stageOpen = false;
+            UI.GetComponent<Text>().text = "\n Chase after Sanchez Mazas!";
             mazasController.startRunning();
+            UI.GetComponent<Text>().text = "\n Press 7 to tell Sanchez Mazas to stop running.";
             dialogueManager.startDialogue(6);
-            //start event 2
         }
         //tell Sanchez Mazas to stop
         if (currStage == stage.dialogue7 && stageOpen)
         {
             stageOpen = false;
+            UI.GetComponent<Text>().text = "\n Press 7 to tell Sanchez Mazas to stop running.";
             dialogueManager.startDialogue(6);
         }
         if (currStage == stage.voiceOver4 && stageOpen)
@@ -202,6 +215,7 @@ public class checkpointManager : MonoBehaviour
     IEnumerator waitForTime(float seconds)
     {
         yield return new WaitForSeconds(seconds);
+        Debug.Log("done waiting");
         currStage += 1;
         stageOpen = true;
     }
