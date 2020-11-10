d547e15ed26a4ba927e0771eb19b5fd45edd6302
Christine Yang <christine.yang@yale.edu>
2020-11-10 10:48:11 -0500

    Panels (#51)
    created materials for the panel display, added 2 info panels to VerbPrep scene, added a script cycleMaterials that cycles the gameobject that it's on through a series of materials at regular intervals

---
+++ SpanishEnvironment1/Assets/CreatedAssets/PanelImages/cycleMaterials.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// takes array of Materials and cycles gameObject that has the script through x specified materials per second
+public class cycleMaterials : MonoBehaviour
+{
+    public Material[] frames;
+    private float framesPerSecond = 1.0f;
+    private float index;
+
+    void Update() 
+    {
+        index = Time.time * framesPerSecond;
+        index = index % frames.Length;
+        gameObject.GetComponent<Renderer>().material = frames[(int)index];    
+    }
+}
